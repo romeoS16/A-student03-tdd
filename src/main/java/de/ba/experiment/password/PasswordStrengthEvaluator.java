597a2ac5a342d1854e7bf4295b71fc933778f903
Anna Vollmer <an591vol@htwg-konstanz.de>
2025-12-18 12:57:41 +0100

    Aufgabe3

--- src/main/java/de/ba/experiment/password/PasswordStrengthEvaluator.java
+++ src/main/java/de/ba/experiment/password/PasswordStrengthEvaluator.java
@@ -4,7 +4,7 @@ public class PasswordStrengthEvaluator {
 
     /**
      * Bewertet die Stärke eines Passworts.
-     *
+     * <p>
      * (Siehe ausführliche Beschreibung in README.md)
      *
      * @param password das zu prüfende Passwort
@@ -12,7 +12,32 @@ public class PasswordStrengthEvaluator {
      */
 
     public static String evaluateStrength(String password) {
-        // TODO: implementieren
+        int points = 0;
+        int length = password.length();
+
+        if (length == 0 || password == null || password.contains(" ") || length < 6) {
+            return "WEAK";
+        } else {
+            if (password.matches("[a-z]+")) {
+                points++;
+            } else if (password.matches("[A-Z]+")) {
+                points++;
+            } else if (password.matches("[0-9]+")) {
+                points++;
+            } else if (password.matches("[\\W]")) {
+                points++;
+            } else if (length >= 10) {
+                points++;
+
+            }
+        }
+        if (points <= 2) {
+            return "WEAK";
+        } else if (points == 3) {
+            return "MEDIUM";
+        } else if (points >= 4) {
+            return "STRONG";
+        }
         return "WEAK";
     }
 }
--- src/test/java/de/ba/experiment/password/PasswordStrengthEvaluatorTest.java
+++ src/test/java/de/ba/experiment/password/PasswordStrengthEvaluatorTest.java
@@ -16,5 +16,81 @@ public class PasswordStrengthEvaluatorTest {
         assertEquals("WEAK", result);
     }
 
-    // TODO: weitere Tests ergänzen
+    @Test (expected = NullPointerException.class)
+    public void null1Password_isWeak() {
+        String result = PasswordStrengthEvaluator.evaluateStrength(null);
+        assertEquals("WEAK", result);
+    }
+
+    @Test
+    public void emptyStringPassword_isWeak() {
+        String result = PasswordStrengthEvaluator.evaluateStrength("");
+        assertEquals("WEAK", result);
+    }
+
+    @Test
+    public void spacePassword_isWeak() {
+        String result = PasswordStrengthEvaluator.evaluateStrength(" ");
+        assertEquals("WEAK", result);
+    }
+
+    @Test
+    public void shortFiveCharsPassword_isWeak() {
+        String result = PasswordStrengthEvaluator.evaluateStrength("45!21");
+        assertEquals("WEAK", result);
+    }
+
+    @Test
+    public void oneFromEverythingAndElevenCharsPassword_isStrong() {
+        String result = PasswordStrengthEvaluator.evaluateStrength("aA11aA11aA11a");
+        assertEquals("STRONG", result);
+    }
+
+    @Test
+    public void oneUpperCasePassword_isWeak() {
+        String result = PasswordStrengthEvaluator.evaluateStrength("Aaaaaaaa");
+        assertEquals("WEAK", result);
+    }
+
+    @Test
+    public void oneLowerCasePassword_isWeak() {
+        String result = PasswordStrengthEvaluator.evaluateStrength("aAAAAAAAA");
+        assertEquals("WEAK", result);
+    }
+
+    @Test
+    public void oneNumberPassword_isWeak() {
+        String result = PasswordStrengthEvaluator.evaluateStrength("aaaa1aaaa");
+        assertEquals("WEAK", result);
+    }
+
+    @Test
+    public void oneNumberAndTenCharsPassword_isStrong() {
+        String result = PasswordStrengthEvaluator.evaluateStrength("aaaa1aaaaa");
+        assertEquals("STRONG", result);
+    }
+
+    @Test
+    public void oneSpecialThingPassword_isWeak() {
+        String result = PasswordStrengthEvaluator.evaluateStrength("aaa!aaa");
+        assertEquals("WEAK", result);
+    }
+
+    @Test
+    public void tenLowerCasePassword_isWeak() {
+        String result = PasswordStrengthEvaluator.evaluateStrength("aaaaaa");
+        assertEquals("WEAK", result);
+    }
+
+    @Test
+    public void noLowerCasePassword_isStrong() {
+        String result = PasswordStrengthEvaluator.evaluateStrength("ASD456QW!!!456");
+        assertEquals("STRONG", result);
+    }
+
+    @Test
+    public void perfectPassword_isStrong() {
+        String result = PasswordStrengthEvaluator.evaluateStrength("asdQEW!$???456");
+        assertEquals("STRONG", result);
+    }
 }
