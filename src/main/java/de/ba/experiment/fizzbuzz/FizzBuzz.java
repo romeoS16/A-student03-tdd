efb38bc4847d229df6e901867a4b52e854089019
Anna Vollmer <an591vol@htwg-konstanz.de>
2025-12-18 11:53:16 +0100

    Aufgabe1

--- src/main/java/de/ba/experiment/fizzbuzz/FizzBuzz.java
+++ src/main/java/de/ba/experiment/fizzbuzz/FizzBuzz.java
@@ -12,7 +12,15 @@ public class FizzBuzz {
      */
 
     public static String[] fizzBuzz(int n) {
-        // TODO: implementieren
-        return new String[0];
+        if(n % 3 == 0 && n % 5 != 0){
+            return new String[]{"Fizz"};
+        } else if (n <= 0) {
+            throw new IllegalArgumentException("Not allowed");
+        } else if (n % 5 == 0 && n % 3 != 0) {
+            return new String[]{"Buzz"};
+        } else if (n % 5 == 0 && n % 3 == 0) {
+            return new String[]{"FizzBuzz"};
+        }
+        return new String[]{String.valueOf(n)};
     }
 }
--- src/test/java/de/ba/experiment/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/de/ba/experiment/fizzbuzz/FizzBuzzTest.java
@@ -17,4 +17,40 @@ public class FizzBuzzTest {
     }
 
     // TODO: weitere Tests schreiben
+    @Test
+    public void fizzBuzzOfThree_returnsArrayWithFizz(){
+        String[] result = FizzBuzz.fizzBuzz(3);
+        assertArrayEquals(new String[]{"Fizz"}, result);
+    }
+
+    @Test
+    public void fizzBuzzOfFive_returnsArrayWithBuzz(){
+        String[] result = FizzBuzz.fizzBuzz(5);
+        assertArrayEquals(new String[]{"Buzz"}, result);
+    }
+
+    @Test
+    public void fizzBuzzOfThreeTimesFive_returnsArrayWithFizzBuzz(){
+        String[] result = FizzBuzz.fizzBuzz(15);
+        assertArrayEquals(new String[]{"FizzBuzz"}, result);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void fizzBuzzOfZero_returnsArrayWithZero(){
+        String[] result = FizzBuzz.fizzBuzz(0);
+        assertArrayEquals(new String[]{"0"}, result);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void fizzBuzzOfNegativeNumber_returnsException(){
+        String[] result = FizzBuzz.fizzBuzz(-1);
+        assertArrayEquals(new String[]{"0"}, result);
+    }
+
+    @Test
+    public void fizzBuzzOfFortyTwo_returnsArrayWithFizz(){
+        String[] result = FizzBuzz.fizzBuzz(42);
+        assertArrayEquals(new String[]{"Fizz"}, result);
+    }
+    
 }
