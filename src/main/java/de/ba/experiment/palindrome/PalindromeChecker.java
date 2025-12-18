6ccee98089703eb08b6aae934e7ca8ce0fadb397
Anna Vollmer <an591vol@htwg-konstanz.de>
2025-12-18 12:21:46 +0100

    Aufgabe2

--- src/main/java/de/ba/experiment/palindrome/PalindromeChecker.java
+++ src/main/java/de/ba/experiment/palindrome/PalindromeChecker.java
@@ -4,9 +4,9 @@ public class PalindromeChecker {
 
     /**
      * Prüft, ob ein gegebener String ein Palindrom ist.
-     *
+     * <p>
      * Ein Palindrom liest sich vorwärts und rückwärts gleich.
-     *
+     * <p>
      * (Siehe ausführliche Beschreibung in README.md)
      *
      * @param text der zu prüfende Text (kann null sein)
@@ -14,7 +14,26 @@ public class PalindromeChecker {
      */
 
     public static boolean isPalindrome(String text) {
-        // TODO: implementieren
+        text = text.replaceAll(" +","");
+        String lowerCaseString = text.toLowerCase();
+        int length = lowerCaseString.length();
+
+        if (length == 0) {
+            return true;
+        } else if (text == null) {
+            return false;
+        }
+
+        for (int i = 0; i < length; i++) {
+            if (lowerCaseString.charAt(i) == lowerCaseString.charAt(length - 1)) {
+                i++;
+                continue;
+            } else if (lowerCaseString.charAt(i) != lowerCaseString.charAt(length - 1)) {
+                return false;
+            }
+            return true;
+        }
+
         return false;
     }
 }
--- src/test/java/de/ba/experiment/palindrome/PalindromeCheckerTest.java
+++ src/test/java/de/ba/experiment/palindrome/PalindromeCheckerTest.java
@@ -2,6 +2,7 @@ package de.ba.experiment.palindrome;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class PalindromeCheckerTest {
@@ -16,5 +17,46 @@ public class PalindromeCheckerTest {
         assertTrue(result);
     }
 
-    // TODO: weitere Testfälle ergänzen
+    @Test
+    public void simpleLowercasePalindrome_returnsFasle() {
+        boolean result = PalindromeChecker.isPalindrome("anlöna");
+        assertFalse(result);
+    }
+
+    @Test
+    public void camelCasePalindrome_returnsTrue(){
+        boolean result = PalindromeChecker.isPalindrome("AkLöLKa");
+        assertTrue(result);
+    }
+
+    @Test
+    public void spacesIgnoredPalindrome_returnsTrue(){
+        boolean result = PalindromeChecker.isPalindrome("kjh kjh  hjk    hjk");
+        assertTrue(result);
+    }
+
+    @Test
+    public void differentSignsPalindrome_returnsTrue(){
+        boolean result = PalindromeChecker.isPalindrome("asd789!==!987dsa");
+        assertTrue(result);
+    }
+
+    @Test
+    public void differentSignsPalindrome_returnsFalse(){
+        boolean result = PalindromeChecker.isPalindrome("asd789=!987dsa");
+        assertFalse(result);
+    }
+
+    @Test
+    public void emptyStringPalindrome_returnsTrue(){
+        boolean result = PalindromeChecker.isPalindrome("");
+        assertTrue(result);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void nullPalindrome_returnsFalse(){
+        boolean result = PalindromeChecker.isPalindrome(null);
+        assertFalse(result);
+    }
+
 }
